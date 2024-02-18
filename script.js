350f5ff417af45c88b846ac679b7c768e4a9df0a
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-18 20:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function getFormvalue() {
     //Write your code here
-	let ans = document.querySelector("#fname").value + " "+ document.querySelector("#lname");
+	let inp = form.querySelectorAll("input");
+	let f_name = inp[0].value;
+	let l_name = inp[1].value;
+	let ans = f_name + " " + l_name;
 	alert(ans);
 
 }
